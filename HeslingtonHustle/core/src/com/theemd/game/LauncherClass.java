b61d64f8eb7e0971e0f71ec6f54aaf0f750e92dd
EreifeOluwasimi Odusi <31220003+Ereife2@users.noreply.github.com>
2024-03-17 05:34:32 +0000

    Splash Screen
    Added splash screen, merging main branch with my own work

--- HeslingtonHustle/core/src/com/theemd/game/LauncherClass.java
+++ HeslingtonHustle/core/src/com/theemd/game/LauncherClass.java
@@ -16,7 +16,7 @@ public class LauncherClass extends Game {
         batch = new SpriteBatch();
         font = new BitmapFont();
         // Creates the first instance of the Main Menu Screen and then displays it.
-        this.setScreen(new MainMenuScreen(this));
+        this.setScreen(new Splash(this));
     }
 
     public void render() {
