da69e091046c55a7b5ad10526a7d6dfa37151705
EreifeOluwasimi Odusi <31220003+Ereife2@users.noreply.github.com>
2024-03-20 04:22:46 +0000

    squares

---
+++ HeslingtonHustle/assets/Squaremap/roguelikeCity_magenta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="roguelikeCity_magenta" tilewidth="32" tileheight="32" tilecount="266" columns="19">
+ <image source="Roguelike Modern City pack/Spritesheet/roguelikeCity_magenta.png" width="628" height="475"/>
+</tileset>
