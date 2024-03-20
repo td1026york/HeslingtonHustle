1b43e1bca441bf6b2ec2519f71c23d4174e97df1
td1026york <160610243+td1026york@users.noreply.github.com>
2024-03-20 18:07:21 +0000

    The Purge
    Removed Unneeded Assets

--- HeslingtonHustle/assets/Squaremap/Roguelike Modern City pack/Spritesheet/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilemap" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="117" columns="13">
- <image source="../../kenney_rpg-urban-pack/Tilemap/tilemap.png" width="458" height="305"/>
-</tileset>
