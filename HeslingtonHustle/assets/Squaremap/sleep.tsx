8cb06a43c47847083ace3590d9333c64d7f1e1b8
td1026york <160610243+td1026york@users.noreply.github.com>
2024-03-20 17:10:13 +0000

    added sleep icon
    sleep icon for interactions map layer

---
+++ HeslingtonHustle/assets/Squaremap/sleep.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="sleep" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <properties>
+  <property name="sleep" value="sleep"/>
+ </properties>
+ <image source="sleep.png" width="16" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="sleep" value="sleep"/>
+  </properties>
+ </tile>
+</tileset>
